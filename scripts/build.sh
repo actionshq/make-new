a680428f460e89a8f65cd5d0e13b9a7ea328d229
Marek Brieger <m.brieger@codevoy.com>
2023-01-08 23:00:28 +0000

    chore: rename group to actionshq

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 TAG=$(git describe --tags --dirty --always)
-BASE_IMAGE="ghcr.io/actionhq/new/app"
+BASE_IMAGE="ghcr.io/actionshq/new/app"
 
 docker build . -t "${BASE_IMAGE}:${TAG}"
--- scripts/stage.sh
+++ scripts/stage.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 TAG=$(git describe --tags --dirty --always)
-BASE_IMAGE="ghcr.io/actionhq/new/app"
+BASE_IMAGE="ghcr.io/actionshq/new/app"
 
 docker tag "${BASE_IMAGE}:${TAG}" "${BASE_IMAGE}:main"
 docker push "${BASE_IMAGE}:main"
