162ccbc149a6625c7670963d3eecd64e7aae3f50
Marek Brieger <m.brieger@codevoy.com>
2023-01-09 19:33:53 +0000

    chore: rename project to make-new

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 TAG=$(git describe --tags --dirty --always)
-BASE_IMAGE="ghcr.io/actionshq/new/app"
+BASE_IMAGE="ghcr.io/actionshq/make-new/app"
 
 docker build . -t "${BASE_IMAGE}:${TAG}"
