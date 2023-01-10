ac0e300ae19569ca888c2f4831ee8b649f958e82
Marek Brieger <m.brieger@codevoy.com>
2023-01-10 21:56:03 +0000

    fix: hide directory button on small screens

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,7 +4,7 @@
             <nav class="flex justify-between">
                 <Logo :actions="actions" />
                 <div class="flex items-center gap-x-5 md:gap-x-8">
-                    <a class="rounded-full py-2 px-4 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+                    <a class="hidden md:inline rounded-full py-2 px-4 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
                         href="/#">Directory</a>
                     <a class="rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:bg-blue-500  focus-visible:outline-blue-600"
                         href="/#">How to use</a>
