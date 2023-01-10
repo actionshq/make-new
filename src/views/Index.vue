29c5d475aec465094ec60d81dc7147aebcb7da63
Marek Brieger <m.brieger@codevoy.com>
2023-01-10 21:19:54 +0000

    feat: add coming soon section

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -12,6 +12,18 @@
             </nav>
         </div>
     </header>
+    <main>
+        <div class="bg-white">
+            <div class="mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8">
+                <div class="text-center">
+                    <p class="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">Coming soon</p>
+                    <p class="mx-auto mt-5 max-w-xl text-xl text-gray-500">
+                        The last link to remember to <u>make.new</u> things.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
