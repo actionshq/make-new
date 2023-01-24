2c0f1b84dbeffe98ec26a0fc49bbb7fbc805e42e
Marek Brieger <m.brieger@codevoy.com>
2023-01-24 21:09:00 +0000

    feat: add offline pwa

--- src/renderer/_default.page.server.ts
+++ src/renderer/_default.page.server.ts
@@ -15,14 +15,10 @@ export async function render(pageContext: PageContextBuiltIn) {
     <html lang="en">
       <head>
         <meta charset="UTF-8">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2563eb">
-        <meta name="theme-color" content="#2563eb">
+        <link rel="manifest" href="/manifest.webmanifest">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="noindex,nofollow">
+        <script>if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}</script>
         <title>make.new</title>
       </head>
       <body class="min-w-[320px]">
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,45 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import ssr from "vite-plugin-ssr/plugin"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  plugins: [vue(), ssr({ prerender: true })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      base: "/",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,woff2,ttf,ico}"],
+      },
+      manifest: {
+        name: "make.new",
+        short_name: "make.new",
+        display: "standalone",
+        theme_color: "#2563eb",
+        background_color: "#2563eb",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+    ssr({ prerender: true }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
