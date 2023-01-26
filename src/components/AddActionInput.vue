556c79bc86e34d3c5b6319c3e53b8de7a9507c4c
Marek Brieger <m.brieger@codevoy.com>
2023-01-26 20:11:01 +0000

    refactor: use blue border on focus for whole AddActionInput

--- src/components/AddActionInput.vue
+++ src/components/AddActionInput.vue
@@ -1,8 +1,10 @@
 <template>
   <form class="mt-1 flex rounded-md shadow-sm" @submit.prevent="onSubmit">
-    <div class="relative flex flex-grow items-stretch focus-within:z-10">
+    <div
+      class="relative flex flex-grow items-stretch rounded-l-md border focus-within:border-blue-600"
+    >
       <span
-        class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm"
+        class="inline-flex items-center border-r select-none bg-gray-50 rounded-l-md px-3 text-gray-500 sm:text-sm"
       >
         make.new&nbsp;/
       </span>
@@ -11,12 +13,12 @@
         autocomplete="off"
         autofocus
         v-model="inputValue"
-        class="block w-full border-gray-300 focus:border-blue-600 focus:ring-0 sm:text-sm"
+        class="block w-full border-none focus:ring-0 sm:text-sm"
       />
     </div>
     <button
       type="submit"
-      class="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-blue-600 bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-500 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+      class="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-blue-600 bg-blue-600 px-4 py-2 text-sm font-medium select-none text-white hover:bg-blue-500 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
     >
       add
     </button>
