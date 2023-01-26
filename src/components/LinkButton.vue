7bb85fc18fc5cf823031a34b8d05af37baa5e52e
Marek Brieger <m.brieger@codevoy.com>
2023-01-26 20:15:02 +0000

    feat!: add plain type to LinkButton

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,9 +1,5 @@
 <template>
-  <a
-    class="rounded-full py-2 px-4 text-sm text-center"
-    :class="classes"
-    :href="href"
-  >
+  <a :class="classes" :href="href">
     <slot />
   </a>
 </template>
@@ -13,13 +9,14 @@ import { computed } from "vue"
 
 const props = defineProps<{
   href: string
-  type: "plain" | "primary"
+  type: "plain" | "primary" | "flat"
 }>()
 
 const typeClasses: Record<(typeof props)["type"], string> = {
-  plain: "text-slate-700 hover:bg-slate-100 hover:text-slate-900",
+  plain: "text-sm text-center text-gray-500 hover:text-gray-900",
+  flat: "rounded-full py-2 px-4 text-sm text-center text-gray-500 hover:text-gray-900 hover:bg-slate-100",
   primary:
-    "text-white font-semibold bg-blue-600 hover:bg-blue-500 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600",
+    "rounded-full py-2 px-4 text-sm text-center text-white font-semibold bg-blue-600 hover:bg-blue-500 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600",
 }
 
 const classes = computed(() => typeClasses[props.type])
